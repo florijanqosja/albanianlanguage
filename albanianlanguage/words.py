80af885b0ad40e2df0e59e423286f291c707174f
florijanqosja <fqo@levelft.com>
2024-05-10 15:50:17 +0100

    Refactor get_all_words function to avoid duplicate entries

--- albanianlanguage/words.py
+++ albanianlanguage/words.py
@@ -2,7 +2,6 @@ import csv
 import ast
 import pkg_resources
 
-
 def get_all_words(starts_with=None, includes=None, return_type=False, return_definition=False):
     """
     Reads words and their details from a CSV file and optionally filters them based on provided criteria. 
@@ -19,23 +18,29 @@ def get_all_words(starts_with=None, includes=None, return_type=False, return_def
     list: Depending on the parameters, returns either a list of words or a list of dictionaries with word details.
     """
     words = []
+    seen_words = set()
     filename = pkg_resources.resource_filename('albanianlanguage', 'words.csv')
 
     with open(filename, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             word = row['word']
-            # Check if the word meets the 'starts_with' or 'includes' condition
             if (starts_with and not word.startswith(starts_with)) or (includes and includes not in word):
                 continue
             
             if return_type or return_definition:
-                word_details = {'word': word}
-                if return_type and 'type' in row:
-                    word_details['type'] = row['type']
-                if return_definition and 'definition' in row:
-                    word_details['definition'] = ast.literal_eval(row['definition'])
-                words.append(word_details)
+                if word not in seen_words:
+                    word_details = {'word': word}
+                    if return_type and 'type' in row:
+                        
+                        word_details['type'] = row['type']
+                    if return_definition and 'definition' in row:
+                        word_details['definition'] = ast.literal_eval(row['definition'])
+                    words.append(word_details)
+                    seen_words.add(word)
             else:
-                words.append(word)
+                if word not in seen_words:
+                    words.append(word)
+                    seen_words.add(word)
+
     return words
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='albanianlanguage',
-    version='0.1.3',
+    version='0.1.4',
     packages=find_packages(),
     install_requires=[
         # none so fas
